4c6f95873c70d130d8a224ab587134be48dbb6f9
mialsu <mialsu@utu.fi>
2021-01-20 18:39:43 +0200

    GUI finished

--- gui_digit_recognizer.py
+++ gui_digit_recognizer.py
@@ -1,5 +1,4 @@
 # Import libraries
-
 import os
 import PIL
 import cv2
@@ -41,7 +40,7 @@ def recognize_digit():
     x = win.winfo_rootx()+widget.winfo_x()
     y = win.winfo_rooty()+widget.winfo_y()
     x1 = x+widget.winfo_width()
-    y1 = y+widget.winfo_width()
+    y1 = y+widget.winfo_height()
 
     #grab the image and crop it, save in png
     ImageGrab.grab().crop((x,y,x1,y1)).save('image.png')
@@ -51,16 +50,47 @@ def recognize_digit():
     # Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply OTSU thresholding
-    ret, th = cv2.threshold(gray,0,255,cv2.TRESH_BINARY_INV+cv2.THRESH_OTSU)
+    ret, th = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     # findContours() function helps to extract contours from image
     contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
     for cnt in contours:
-        #Get bounding box, extract ROI
+        # Get bounding box, extract ROI
         x,y,w,h = cv2.boundingRect(cnt)
+        # Create rectangle
+        cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 1)
+        top = int(0.05*th.shape[0])
+        bottom = top
+        left = int(0.05*th.shape[1])
+        right = left
+        th_up = cv2.copyMakeBorder(th, top, bottom, left, right, cv2.BORDER_REPLICATE)
+        #Extract the image ROI
+        roi = th[y-top:y+h+bottom,x-left:x+w+right]
+        #Resize image to 28x28 pixels
+        img = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+        # Reshape image
+        img = img.reshape(1,28,28,1)
+        # Normalize image
+        img = img/255.0
+        # Predict result
+        pred = model.predict([img])[0]
+        # Return indices of max values
+        final_pred = np.argmax(pred)
 
+        # draw text on image
+        data = str(final_pred)+'  '+str(int(max(pred)*100))+'%'
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.5
+        color = (255, 0, 0)
+        thickness = 1
+        cv2.putText(image, data, (x, y-5), font, fontScale, color, thickness)
+    
+    # Show results in new window, close window after 10 seconds
+    cv2.imshow('image', image)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
 
-
+# load model
 model = load_model("mnist.h5")
 print("Model loaded succesfully, starting GUI.")
 
